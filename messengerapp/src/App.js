b8140e329d85867124a49e55d6994aa3e0348614
Andres Icedo <andresicedo24@gmail.com>
2021-02-23 20:00:37 -0600

    updated code for messenger

--- messengerapp/src/App.js
+++ messengerapp/src/App.js
@@ -1,19 +1,26 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+
 const App = () => {
-    return (
-        <ChatEngine 
-            height="100vh"
-            projectID="f646c1d7-fb85-48e5-bd73-96adbe1f9371"
-            userName="andresIcedo"
-            userSecret="raven2021"
-            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-        />
-    );
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
